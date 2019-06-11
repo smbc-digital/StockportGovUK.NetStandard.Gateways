3eb25f168c986a521160e90ed9d238e046f21081
stephen.lally <stephen.lally.smbc@gmail.com>
2019-06-11 17:28:47 +0100

    fix(FosteringService): Updated interface

--- FosteringServiceGateway/IFosteringServiceGateway.cs
+++ FosteringServiceGateway/IFosteringServiceGateway.cs
@@ -1,10 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Threading.Tasks;
+using StockportGovUK.AspNetCore.Gateways.Response;
+using StockportGovUK.NetStandard.Models.Models.Verint;
 
 namespace StockportGovUK.AspNetCore.Gateways.FosteringServiceGateway
 {
     public interface IFosteringServiceGateway
     {
+        Task<HttpResponse<Case>> GetCase(string caseRef);
     }
 }
