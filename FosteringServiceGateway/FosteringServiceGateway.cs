5690253306afc20bad809dba16b5162bea32a80b
stephen.lally <stephen.lally.smbc@gmail.com>
2019-06-11 17:25:38 +0100

    feat(FosteringService): Added fostering service gateway

---
+++ FosteringServiceGateway/FosteringServiceGateway.cs
@@ -0,0 +1,22 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using StockportGovUK.AspNetCore.Gateways.Response;
+using StockportGovUK.NetStandard.Models.Models.Verint;
+
+namespace StockportGovUK.AspNetCore.Gateways.FosteringServiceGateway
+{
+    public class FosteringServiceGateway : Gateway, IFosteringServiceGateway
+    {
+        private const string HttpClientName = "fosteringServiceGateway";
+        private const string CaseEndpoint = "api/v1/Fostering";
+
+        public FosteringServiceGateway(IHttpClientFactory clientFactory) : base(clientFactory)
+        {
+        }
+
+        public async Task<HttpResponse<Case>> GetCase(string caseRef)
+        {
+            return await GetAsync<Case>(HttpClientName, $"{CaseEndpoint}/case?caseId={caseRef}");
+        }
+    }
+}
---
+++ FosteringServiceGateway/IFosteringServiceGateway.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockportGovUK.AspNetCore.Gateways.FosteringServiceGateway
+{
+    public interface IFosteringServiceGateway
+    {
+    }
+}
