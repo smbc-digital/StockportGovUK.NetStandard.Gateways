4f96a38f16aec823c58979f6c30d04cee6799c8f
stephen.lally <stephen.lally.smbc@gmail.com>
2019-06-19 16:53:02 +0100

    chore(Gateway): Removed encoding and type from stringContent

--- Gateway.cs
+++ Gateway.cs
@@ -36,7 +36,7 @@ namespace StockportGovUK.AspNetCore.Gateways
 
         public async Task<HttpResponseMessage> PatchAsync(string name, string url, object content)
         {
-            var stringContent = new StringContent(content.ToString(), System.Text.Encoding.UTF8, "application/x-www-form-urlencoded");
+            var stringContent = new StringContent(content.ToString());
 
             var client = _clientFactory.CreateClient(name);
             return await client.PatchAsync(url, stringContent);
