cab1783b97d75e647d7fff8d170dac97181a6583
Lydia Gray <lydia.gray@stockport.gov.uk>
2019-09-02 10:01:32 +0100

    fix(compliments): fix namespace and classname

--- ComplimentsComplaintsServiceGateway/ComplimentsComplaintsServiceGateway.cs
+++ ComplimentsComplaintsServiceGateway/ComplimentsComplaintsServiceGateway.cs
@@ -1,20 +1,16 @@
 using System.Net.Http;
 using System.Threading.Tasks;
 using StockportGovUK.AspNetCore.Gateways.Response;
-using StockportGovUK.NetStandard.Models.Enums;
 using StockportGovUK.NetStandard.Models.Models;
-using StockportGovUK.NetStandard.Models.Models.Fostering;
-using StockportGovUK.NetStandard.Models.Models.Fostering.HomeVisit;
-using StockportGovUK.NetStandard.Models.Models.Fostering.Application;
 
-namespace StockportGovUK.AspNetCore.Gateways.ComplimentsComplaintsGateway
+namespace StockportGovUK.AspNetCore.Gateways.ComplimentsComplaintsServiceGateway
 {
-    public class ComplimentsComplaintsGateway : Gateway, IComplimentsComplaintsGateway
+    public class ComplimentsComplaintsServiceGateway : Gateway, IComplimentsComplaintsServiceGateway
     {
         private const string HttpClientName = "complimentsComplaintsGateway";
         private const string ComplimentEndpoint = "api/v1/Compliment";
 
-        public ComplimentsComplaintsGateway(IHttpClientFactory clientFactory) : base(clientFactory)
+        public ComplimentsComplaintsServiceGateway(IHttpClientFactory clientFactory) : base(clientFactory)
         {
         }
 
