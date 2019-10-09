d69a4ea63619016283329c78af20deed700cc25b
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2019-10-09 13:43:11 +0100

    fix(netcall gateway) - Fix faulty url

--- NetcallGateway/NetcallGateway.cs
+++ NetcallGateway/NetcallGateway.cs
@@ -15,11 +15,11 @@ namespace StockportGovUK.AspNetCore.Gateways.Netcall
 
         public async Task<HttpResponseMessage> PauseRecordingAsync(string netcallUserId)
         {
-            return await GetAsync($"{NetcallEndpoint}/pauserecording/{netcallUserId}?type=UserID&respond=true");
+            return await GetAsync($"{NetcallEndpoint}/pauserecording/{netcallUserId}?type=UserID?respond=true");
         }
         
         public async Task<HttpResponseMessage> ResumeRecordingAsync(string netcallUserId){
-            return await GetAsync($"{NetcallEndpoint}/resumerecording/{netcallUserId}?type=UserID&respond=true");
+            return await GetAsync($"{NetcallEndpoint}/resumerecording/{netcallUserId}?type=UserID?respond=true");
         }
     }
 }
