464996fa371bcdfb5aae634216bf75c18a493325
Lydia Gray <lydia.gray@stockport.gov.uk>
2019-10-23 14:32:12 +0100

    feature(revsBensService): add RevsBensServiceGateway

---
+++ RevsBensServiceGateway/IRevsBensServiceGateway.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StockportGovUK.AspNetCore.Gateways.RevsBensServiceGateway
+{
+    public interface IRevsBensServiceGateway
+    {
+    }
+}
---
+++ RevsBensServiceGateway/RevsBensServiceGateway.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+
+namespace StockportGovUK.AspNetCore.Gateways.RevsBensServiceGateway
+{
+    public class RevsBensServiceGateway : Gateway, IRevsBensServiceGateway
+    {
+        public RevsBensServiceGateway(HttpClient httpClient) : base(httpClient)
+        {
+        }
+    }
+}
