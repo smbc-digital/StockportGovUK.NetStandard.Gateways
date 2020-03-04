c7c79992a46e99428fb9fc7a401cdeb077106bfb
Rowan Dennigan <rowan.dennigan@stockport.gov.uk>
2020-03-04 16:10:51 +0000

    feature(OrganisationService): added organisation service gateway

---
+++ OrganisationServiceGateway/IOrganisationServiceGateway.cs
@@ -0,0 +1,13 @@
+﻿using StockportGovUK.NetStandard.Gateways.Response;
+using StockportGovUK.NetStandard.Models.Street;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using StockportGovUK.NetStandard.Models.Verint.Lookup;
+
+namespace StockportGovUK.NetStandard.Gateways.StreetServiceGateway
+{
+    public interface IOrganisationServiceGateway
+    {
+        Task<HttpResponse<IEnumerable<OrganisationSearchResult>>> SearchAsync(OrganisationSearch model);
+    }
+}
---
+++ OrganisationServiceGateway/OrganisationServiceGateway.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Extensions.Logging;
+using StockportGovUK.NetStandard.Gateways.Response;
+using StockportGovUK.NetStandard.Models.Street;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using StockportGovUK.NetStandard.Models.Verint.Lookup;
+
+namespace StockportGovUK.NetStandard.Gateways.StreetServiceGateway
+{
+    public class OrganisationServiceGateway : Gateway, IOrganisationServiceGateway
+    {
+        public OrganisationServiceGateway(HttpClient httpClient, ILogger<Gateway> logger) : base(httpClient, logger)
+        {
+        }
+
+        public async Task<HttpResponse<IEnumerable<OrganisationSearchResult>>> SearchAsync(OrganisationSearch model)
+        {
+            return await GetAsync<IEnumerable<OrganisationSearchResult>>($"api/v1/Street/{model.OrganisationProvider}/{model.SearchTerm}");
+        }
+    }
+}
