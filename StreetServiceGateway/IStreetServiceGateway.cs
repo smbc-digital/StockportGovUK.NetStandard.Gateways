6148b19740bbb53f2ec7cfb4647fe1a60394623c
stephen.lally <stephen.lally.smbc@gmail.com>
2020-03-04 11:08:56 +0000

    feat(Street): Added street service

---
+++ StreetServiceGateway/IStreetServiceGateway.cs
@@ -0,0 +1,13 @@
+﻿using StockportGovUK.NetStandard.Gateways.Response;
+using StockportGovUK.NetStandard.Models.Addresses;
+using StockportGovUK.NetStandard.Models.Street;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace StockportGovUK.NetStandard.Gateways.StreetServiceGateway
+{
+    public interface IStreetServiceGateway
+    {
+        Task<HttpResponse<IEnumerable<AddressSearchResult>>> SearchAsync(StreetSearch model);
+    }
+}
