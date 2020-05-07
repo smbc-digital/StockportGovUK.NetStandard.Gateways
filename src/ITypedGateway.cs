a7ba8f734c3fdb29d096464ee45ec804e58721a0
Colin Lees <colinleesa7x@gmail.com>
2020-05-07 21:37:00 +0100

    fix: cast objects to HttpContent over StringContent

--- src/ITypedGateway.cs
+++ src/ITypedGateway.cs
@@ -6,21 +6,13 @@ namespace StockportGovUK.NetStandard.Gateways
     interface ITypedGateway
     {
         Task<HttpResponse<T>> GetAsync<T>(string url);
-
         Task<HttpResponse<T>> PatchAsync<T>(string url, object content);
-
         Task<HttpResponse<T>> PatchAsync<T>(string url, object content, bool encodeContent);
-
         Task<HttpResponse<T>> PostAsync<T>(string url, object content);
-
         Task<HttpResponse<T>> PostAsync<T>(string url, object content, bool encodeContent);
-
         Task<HttpResponse<T>> PutAsync<T>(string url, object content);
-
         Task<HttpResponse<T>> PutAsync<T>(string url, object content, bool encodeContent);
-
         Task<HttpResponse<T>> DeleteAsync<T>(string url);
-
         void ChangeAuthenticationHeader(string authHeader);
     }
 }
