a225fc0a82f2dc0a9a6141306c89c16f4f8fe897
lallysmbc <35955528+lallysmbc@users.noreply.github.com>
2020-12-21 09:41:44 +0000

    build: release 5.4.0 (#57)
    * chore(): update model package
    
    * feat(BookingServiceGateway) (#53)
    
    * feat(BookingServiceGateway)
    
    * chore(projcet): updated StockportGovUK.NetStandard.Gateways
    
    * chore(project): updated models package
    
    * Booking service gateway (#54)
    
    * feat(BookingServiceGateway)
    
    * chore(projcet): updated StockportGovUK.NetStandard.Gateways
    
    * chore(project): updated models package
    
    * chore(BookingService): Syntax suggestion and private method
    
    * chore(BookingService): Clean up "using"
    
    Co-authored-by: stephen.lally <stephen.lally.smbc@gmail.com>
    Co-authored-by: Stuart Wilkes <stuart.wilkes@stockport.gov.uk>
    
    * chore(): update models package
    
    * chore(project): update dep StockportGovUK.NetStandard.Models
    
    * fix(booking): change http method for Confirmation
    
    * chore(package): updated dep StockportGovUK.NetStandard.Models
    
    * chore(package): updated test project dep StockportGovUK.NetStandard.Models
    
    * chore(deps): updatd StockportGovUK.Models, Bumped version
    
    * chore(project): updated build number for minor
    
    * Update BookingServiceGateway.cs
    
    Co-authored-by: Harry <harry.woodall@stockport.gov.uk>
    Co-authored-by: michael hayes <michael.hayes@stockport.gov.uk>
    Co-authored-by: Stu <h.s.j.wilkes@gmail.com>
    Co-authored-by: Stuart Wilkes <stuart.wilkes@stockport.gov.uk>
    Co-authored-by: Colin Lees <colin.lees@stockport.gov.uk>

---
+++ src/BookingService/BookingServiceGateway.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using StockportGovUK.NetStandard.Gateways.Response;
+using StockportGovUK.NetStandard.Models.Booking.Request;
+using StockportGovUK.NetStandard.Models.Booking.Response;
+
+namespace StockportGovUK.NetStandard.Gateways.BookingService
+{
+    public class BookingServiceGateway : Gateway, IBookingServiceGateway
+    {
+        private const string AvailabilityEndpoint = "api/v1/Availability";
+        private const string ConfirmationEndpoint = "api/v1/Confirmation";
+        private const string ReservationEndpoint = "api/v1/Reservation";
+        private const string NextAvailabilityAction = "/next-availability";
+
+        public BookingServiceGateway(HttpClient httpClient) : base(httpClient)
+        {
+        }
+
+        public async Task<HttpResponse<List<AvailabilityDayResponse>>> GetAvailability(AvailabilityRequest model) => 
+            await GetAsync<List<AvailabilityDayResponse>>($"{AvailabilityEndpoint}{queryString(model)}");
+
+        public async Task<HttpResponse<AvailabilityDayResponse>> NextAvailability(AvailabilityRequest model) => 
+            await GetAsync<AvailabilityDayResponse>($"{AvailabilityEndpoint}{NextAvailabilityAction}{queryString(model)}");
+
+        public async Task<HttpResponse<Guid>> Reserve(BookingRequest model) =>
+            await PostAsync<Guid>(ReservationEndpoint, model);
+
+        public async Task<HttpResponseMessage> Confirmation(ConfirmationRequest model) =>
+            await PatchAsync(ConfirmationEndpoint, model);
+
+        private string queryString(AvailabilityRequest model) =>
+            $"?{nameof(model.AppointmentId)}={model.AppointmentId}&{nameof(model.StartDate)}={model.StartDate:s}&{nameof(model.EndDate)}={model.EndDate:s}{OptionalResourcesQueryString(model.OptionalResources, nameof(model.OptionalResources))}";
+
+        private string OptionalResourcesQueryString(List<BookingResource> optionalResources, string listOfType) =>
+            optionalResources == null
+                ? string.Empty
+                : optionalResources
+                    .Select((_, index) => $"&{listOfType}[{index}].{nameof(BookingResource.Quantity)}={_.Quantity}&{listOfType}[{index}].{nameof(BookingResource.ResourceId)}={_.ResourceId}")
+                    .Aggregate("", (acc, _) => $"{acc}{_}");
+    }
+}
