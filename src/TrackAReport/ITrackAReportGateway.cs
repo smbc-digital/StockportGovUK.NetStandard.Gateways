ac1ea87dc71a828aed27de8c2d59fa62a4836e08
Stu <h.s.j.wilkes@gmail.com>
2021-02-25 11:45:55 +0000

    feat(track_a_report_D-4751): Add TrackAReport api

---
+++ src/TrackAReport/ITrackAReportGateway.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using System.Net.Http;
+using StockportGovUK.NetStandard.Models.TrackAReport.Requests;
+
+namespace StockportGovUK.NetStandard.Gateways.TrackAReport
+{
+    public interface ITrackAReportGateway
+    {
+        Task<HttpResponseMessage> AssetEnquiry(TrackAReportRequest model);
+    }
+}
