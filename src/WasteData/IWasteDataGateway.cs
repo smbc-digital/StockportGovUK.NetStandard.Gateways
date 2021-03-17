38d0b85ea32d16c81fa71910ef3eeb85d5a60bdd
Lydia Gray <34089008+lydiagray@users.noreply.github.com>
2021-03-17 10:02:21 +0000

    feat(WasteData): add WasteDataService gateway (#94)
    * feat(WasteData): add gateway for WasteDataService
    
    * chore(csproj): patch version bump
    
    * fix(csproj): fix version increment

---
+++ src/WasteData/IWasteDataGateway.cs
@@ -0,0 +1,11 @@
+﻿using System.Threading.Tasks;
+using StockportGovUK.NetStandard.Gateways.Response;
+using StockportGovUK.NetStandard.Models.WasteDataService.Response;
+
+namespace StockportGovUK.NetStandard.Gateways.WasteData
+{
+    public interface IWasteDataGateway
+    {
+        Task<HttpResponse<PropertyWasteData>> Get(string uprn);
+    }
+}
---
+++ src/WasteData/WasteDataGateway.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using StockportGovUK.NetStandard.Gateways.Response;
+using StockportGovUK.NetStandard.Models.WasteDataService.Response;
+
+namespace StockportGovUK.NetStandard.Gateways.WasteData
+{
+    public class WasteDataGateway : Gateway, IWasteDataGateway
+    {
+        private const string WasteDataEndpoint = "api/v1/WasteData";
+
+        public WasteDataGateway(HttpClient httpClient) : base(httpClient) { }
+
+        public async Task<HttpResponse<PropertyWasteData>> Get(string uprn)
+            => await GetAsync<PropertyWasteData>($"{WasteDataEndpoint}?uprn={uprn}");
+    }
+}
