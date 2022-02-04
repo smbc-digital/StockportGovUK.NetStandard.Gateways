87872245d811bbef761272f0ec1eebeecdf9807b
jonhadley <hadley.jk@gmail.com>
2022-02-04 14:48:03 +0000

    Develop (#150)
    * feat(RevsBens): add additional endpoints for currentAccountReference and reducedCouncilTaxDetails
    
    * major: rename badly named IWasteDataGateway WasteCollectionGateway to WasteDataGateway
    
    * patch: fix polly version number
    
    Co-authored-by: Lydia Gray <lydia.gray@stockport.gov.uk>
    Co-authored-by: Stu <h.s.j.wilkes@gmail.com>
    Co-authored-by: Jonathan Hadley <jonathan.hadley@stockport.gov.uk>

--- src/WasteData/WasteDataGateway.cs
+++ src/WasteData/WasteDataGateway.cs
@@ -5,11 +5,11 @@ using StockportGovUK.NetStandard.Models.WasteDataService.Response;
 
 namespace StockportGovUK.NetStandard.Gateways.WasteData
 {
-    public class WasteCollectionGateway : Gateway, IWasteDataGateway
+    public class WasteDataGateway : Gateway, IWasteDataGateway
     {
         private const string WasteDataEndpoint = "api/v1/WasteData";
 
-        public WasteCollectionGateway(HttpClient httpClient) : base(httpClient) { }
+        public WasteDataGateway(HttpClient httpClient) : base(httpClient) { }
 
         public async Task<HttpResponse<PropertyWasteData>> Get(long uprn)
             => await GetAsync<PropertyWasteData>($"{WasteDataEndpoint}?uprn={uprn}");
