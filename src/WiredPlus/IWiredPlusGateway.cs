ba14df189a91577f7cfd760ef7982e588da45e5e
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2022-05-27 15:17:04 +0100

    minor: add basic implementation for WiredPlus gateway

---
+++ src/WiredPlus/IWiredPlusGateway.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using StockportGovUK.NetStandard.Gateways.Response;
+
+namespace StockportGovUK.NetStandard.Gateways.WiredPlus
+{
+    public interface IWiredPlusGateway
+    {
+        Task<HttpResponse<NewsletterSignupResponse>> CreateContact(NewletterSignUpReqest request);
+    }
+}
---
+++ src/WiredPlus/NewsletterSignupResponse.cs
@@ -0,0 +1,8 @@
+namespace StockportGovUK.NetStandard.Gateways.WiredPlus
+{
+    public class NewsletterSignupResponse
+    {
+        public string id { get; set; }
+        public string message { get; set; }
+    }
+}
