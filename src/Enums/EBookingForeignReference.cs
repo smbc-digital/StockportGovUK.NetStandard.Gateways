f40b81d59443c5eadea44fd620cd421efc5454a7
Lydia Gray <lydia.gray@stockport.gov.uk>
2022-08-25 11:13:23 +0100

    chore(BookingConfirmationRequest): add new properties to Model

---
+++ src/Enums/EBookingForeignReference.cs
@@ -0,0 +1,10 @@
+﻿namespace StockportGovUK.NetStandard.Gateways.Enums
+{
+    public enum EBookingForeignReference
+    {
+        VerintRef,
+        UniformRef,
+        ConfirmRef,
+        PaymentRef,
+    }
+}
