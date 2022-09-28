1b9308ab14c812bc28c1ee558022515be798aec3
Lydia Gray <34089008+lydiagray@users.noreply.github.com>
2022-09-28 10:00:38 +0100

    Develop (#187)
    * chore(EBookingForeignReference): add Reference (#185)
    
    * feat(BookingMetaData): add gateway and model for UpdateAdditionalInformationMetaDataValues
    
    * fix(BookingMetaData): fix incorrect parameter
    
    * fix(BookingMetaData): amend plural to singular
    
    Co-authored-by: Chilejon <jon.chiles@stockport.gov.uk>

--- src/BookingService/BookingServiceGateway.cs
+++ src/BookingService/BookingServiceGateway.cs
@@ -47,6 +47,9 @@ namespace StockportGovUK.NetStandard.Gateways.BookingService
         public async Task<HttpResponseMessage> AddReferences(List<AddReferenceRequest> addReferenceRequests) =>
             await PatchAsync($"{BookingEndpoint}/add-references", addReferenceRequests);
 
+        public async Task<HttpResponseMessage> UpdateAdditionalInformationMetaDataValues(UpdateAdditionalInformationMetaDataValuesRequest updateAdditionalInformationMetaDataValuesRequest) =>
+            await PatchAsync($"{BookingEndpoint}/update-additional-information-meta-data-values", updateAdditionalInformationMetaDataValuesRequest);
+
         public async Task<HttpResponse<string>> GetLocation(LocationRequest model) =>
             await GetAsync<string>($"{LocationEndpoint}{LocationQueryString(model)}");
 
