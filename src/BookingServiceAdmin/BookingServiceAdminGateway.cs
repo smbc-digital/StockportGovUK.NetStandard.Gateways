d5818ef2abc24f9ff1dc4ebb774578d83a6bb25a
Lydia Gray <lydia.gray@stockport.gov.uk>
2023-01-18 09:34:06 +0000

    feat(BookingServiceAdminGateway): add endpoints

--- src/BookingServiceAdmin/BookingServiceAdminGateway.cs
+++ src/BookingServiceAdmin/BookingServiceAdminGateway.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace StockportGovUK.NetStandard.Gateways.BookingServiceAdmin
@@ -7,6 +8,8 @@ namespace StockportGovUK.NetStandard.Gateways.BookingServiceAdmin
     {
         private const string ContextEndpoint = "api/v1/Context";
         private const string HomeEndpoint = "api/v1/Home";
+        private const string ResourceEndpoint = "api/v1/Resource";
+        private const string SuspensionEndpoint = "api/v1/Suspension";
         private const string UserEndpoint = "api/v1/User";
 
         public BookingServiceAdminGateway(HttpClient httpClient) : base(httpClient)
@@ -16,10 +19,25 @@ namespace StockportGovUK.NetStandard.Gateways.BookingServiceAdmin
         public async Task<HttpResponseMessage> GetContexts() =>
             await GetAsync($"{ContextEndpoint}");
 
+        public async Task<HttpResponseMessage> GetContext(Guid contextId) =>
+            await GetAsync($"{ContextEndpoint}/{contextId}");
+
         public async Task<HttpResponseMessage> GetVersionNumber() =>
             await GetAsync($"{HomeEndpoint}/version-number");
 
         public async Task<HttpResponseMessage> GetByUsername(string username) =>
             await GetAsync($"{UserEndpoint}/name/{username}");
+
+        public async Task<HttpResponseMessage> GetSuspensionsForContext(Guid contextId) =>
+            await GetAsync($"{SuspensionEndpoint}/{contextId}");
+
+        public async Task<HttpResponseMessage> GetActiveAndFutureSuspensionsForContext(Guid contextId) =>
+            await GetAsync($"{SuspensionEndpoint}/{contextId}/active");
+
+        public async Task<HttpResponseMessage> GetResourceModifiersForContext(Guid contextId) =>
+            await GetAsync($"{ResourceEndpoint}/resource-modifiers/{contextId}");
+
+        public async Task<HttpResponseMessage> GetActiveAndFutureResourceModifiersForContext(Guid contextId) =>
+            await GetAsync($"{SuspensionEndpoint}/resource-modifiers/{contextId}/active");
     }
 }
