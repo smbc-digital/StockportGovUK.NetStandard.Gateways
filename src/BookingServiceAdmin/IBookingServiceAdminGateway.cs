e54dc47771afeead3e3d3c329168ab4b17b6c6bd
Lydia Gray <lydia.gray@stockport.gov.uk>
2023-01-19 08:36:30 +0000

    chore(BookingAdminGateway): update interface

--- src/BookingServiceAdmin/IBookingServiceAdminGateway.cs
+++ src/BookingServiceAdmin/IBookingServiceAdminGateway.cs
@@ -1,4 +1,5 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace StockportGovUK.NetStandard.Gateways.BookingServiceAdmin
@@ -7,8 +8,18 @@ namespace StockportGovUK.NetStandard.Gateways.BookingServiceAdmin
     {
         Task<HttpResponseMessage> GetVersionNumber();
 
+        Task<HttpResponseMessage> GetContext(Guid contextId);
+
         Task<HttpResponseMessage> GetContexts();
 
         Task<HttpResponseMessage> GetByUsername(string username);
+
+        Task<HttpResponseMessage> GetSuspensionsForContext(Guid contextId);
+
+        Task<HttpResponseMessage> GetActiveAndFutureSuspensionsForContext(Guid contextId);
+
+        Task<HttpResponseMessage> GetResourceModifiersForContext(Guid contextId);
+
+        Task<HttpResponseMessage> GetActiveAndFutureResourceModifiersForContext(Guid contextId);
     }
 }
