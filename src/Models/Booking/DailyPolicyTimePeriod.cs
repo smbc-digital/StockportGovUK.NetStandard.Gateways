42477dc4b4e578da7fa41ab4205cdca83198d30c
Lydia Gray <lydia.gray@stockport.gov.uk>
2023-02-14 12:36:11 +0000

    feat(Booking-Policy): add models & gateways for Policy, Daily Policy and Time Period Policy

---
+++ src/Models/Booking/DailyPolicyTimePeriod.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace StockportGovUK.NetStandard.Gateways.Models.Booking;
+
+public class DailyPolicyTimePeriod
+{
+    public Guid DailyPolicyId { get; set; }
+    public Guid TimePeriodPolicyId { get; set; }
+}
---
+++ src/Models/Booking/Request/DailyPolicyRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockportGovUK.NetStandard.Gateways.Models.Booking.Request;
+
+public class DailyPolicyRequest
+{
+    public Guid UserId { get; set; }
+
+    [Required]
+    public DailyPolicy DailyPolicy { get; set; }
+}
