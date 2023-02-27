e44daa79cfc216b6cde3ad64332659f30c79bda4
michaellau-smbc <62244533+michaellau-smbc@users.noreply.github.com>
2023-02-27 14:47:20 +0000

    chore(note): add CreatedBy

--- src/Models/Booking/Note.cs
+++ src/Models/Booking/Note.cs
@@ -9,5 +9,6 @@ public class Note
     public string Text { get; set; }
     public Guid? BookingId { get; set; }
     public Guid? CreatedById { get; set; }
+    public string CreatedBy { get; set; }
     public bool? SystemGenerated { get; set; }
 }
