7aa4d33f97314d55584102368a698015b4074b8d
Jon Chiles <jonchiles@yahoo.com>
2023-02-27 13:04:32 +0000

    chore(uniform): did a control KD to fix formatting conflicts seen in git hub

--- src/UniformService/IUniformServiceGateway.cs
+++ src/UniformService/IUniformServiceGateway.cs
@@ -4,20 +4,20 @@ using StockportGovUK.NetStandard.Gateways.Response;
 
 namespace StockportGovUK.NetStandard.Gateways.UniformService
 {
-    public interface IUniformServiceGateway
-    {
-        Task<HttpResponse<string>> GetPestControlRequest(string id);
-        Task<HttpResponse<string>> CreatePestControlRequest(PestControlServiceRequest request);
-        Task<HttpResponse<string>> ResubmitPestControlRequest(string crmCaseId);
-        Task<HttpResponse<string>> CreateNoiseNuisanceRequest(NoiseNuisanceServiceRequest request);
-        Task<HttpResponse<string>> ResubmitNoiseNuisanceRequest(string crmCaseId);
-        Task<HttpResponse<string>> CreateBonfireNuisanceRequest(BonfireNuisanceServiceRequest request);
-        Task<HttpResponse<string>> ResubmitBonfireNuisanceRequest(string crmCaseId);
-        Task<HttpResponse<string>> CreateHousingDisrepairServiceRequest(HousingDisrepairServiceRequest request);
-        Task<HttpResponse<string>> ResubmitHousingDisrepairServiceRequest(string crmCaseId);
+	public interface IUniformServiceGateway
+	{
+		Task<HttpResponse<string>> GetPestControlRequest(string id);
+		Task<HttpResponse<string>> CreatePestControlRequest(PestControlServiceRequest request);
+		Task<HttpResponse<string>> ResubmitPestControlRequest(string crmCaseId);
+		Task<HttpResponse<string>> CreateNoiseNuisanceRequest(NoiseNuisanceServiceRequest request);
+		Task<HttpResponse<string>> ResubmitNoiseNuisanceRequest(string crmCaseId);
+		Task<HttpResponse<string>> CreateBonfireNuisanceRequest(BonfireNuisanceServiceRequest request);
+		Task<HttpResponse<string>> ResubmitBonfireNuisanceRequest(string crmCaseId);
+		Task<HttpResponse<string>> CreateHousingDisrepairServiceRequest(HousingDisrepairServiceRequest request);
+		Task<HttpResponse<string>> ResubmitHousingDisrepairServiceRequest(string crmCaseId);
 		Task<HttpResponse<string>> CreateTaxiLicenceRequest(TaxiLicensingServiceRequest request);
-        Task<HttpResponse<string>> UpdateTaxiLicenceRequestWithPayment(TaxiLicensingUpdateRequest request);
-        Task<HttpResponse<string>> UpdateTaxiLicenceRequestWithDetails(TaxiLicensingUpdateRequest request);
+		Task<HttpResponse<string>> UpdateTaxiLicenceRequestWithPayment(TaxiLicensingUpdateRequest request);
+		Task<HttpResponse<string>> UpdateTaxiLicenceRequestWithDetails(TaxiLicensingUpdateRequest request);
 		Task<HttpResponse<string>> CheckTaxiLicenceRequestIsOpen(TaxiLicensingUpdateRequest request);
 	}
 }
--- src/UniformService/UniformServiceGateway.cs
+++ src/UniformService/UniformServiceGateway.cs
@@ -5,15 +5,15 @@ using StockportGovUK.NetStandard.Gateways.Response;
 
 namespace StockportGovUK.NetStandard.Gateways.UniformService
 {
-    public class UniformServiceGateway : Gateway, IUniformServiceGateway
-    {
-        private const string PestControlEndpoint = "api/v1/PestControl";
+	public class UniformServiceGateway : Gateway, IUniformServiceGateway
+	{
+		private const string PestControlEndpoint = "api/v1/PestControl";
 
-        private const string NoiseNuisanceEndpoint = "api/v1/NoiseNuisance";
+		private const string NoiseNuisanceEndpoint = "api/v1/NoiseNuisance";
 
-        private const string BonfireNuisanceEndpoint = "api/v1/BonfireNuisance";
+		private const string BonfireNuisanceEndpoint = "api/v1/BonfireNuisance";
 
-        private const string HousingDisrepairEndpoint = "api/v1/HousingDisrepair";
+		private const string HousingDisrepairEndpoint = "api/v1/HousingDisrepair";
 
 		private const string TaxiLicenceEndpoint = "api/v1/TaxiLicensing";
 
@@ -24,35 +24,35 @@ namespace StockportGovUK.NetStandard.Gateways.UniformService
 		private const string TaxiLicenceEndpointCheckIsOpen = "api/v1/TaxiLicensing/CheckIsOpen";
 
 		public UniformServiceGateway(HttpClient httpClient) : base(httpClient)
-        {
-        }
+		{
+		}
 
-        public async Task<HttpResponse<string>> GetPestControlRequest(string id)
-            => await GetAsync<string>($"{PestControlEndpoint}?reference={id}");
+		public async Task<HttpResponse<string>> GetPestControlRequest(string id)
+			=> await GetAsync<string>($"{PestControlEndpoint}?reference={id}");
 
-        public async Task<HttpResponse<string>> CreatePestControlRequest(PestControlServiceRequest request)
-            => await PostAsync<string>($"{PestControlEndpoint}", request);
+		public async Task<HttpResponse<string>> CreatePestControlRequest(PestControlServiceRequest request)
+			=> await PostAsync<string>($"{PestControlEndpoint}", request);
 
-        public async Task<HttpResponse<string>> ResubmitPestControlRequest(string crmCaseId)
-            => await PostAsync<string>($"{PestControlEndpoint}/Resubmit", crmCaseId);
+		public async Task<HttpResponse<string>> ResubmitPestControlRequest(string crmCaseId)
+			=> await PostAsync<string>($"{PestControlEndpoint}/Resubmit", crmCaseId);
 
-        public async Task<HttpResponse<string>> CreateNoiseNuisanceRequest(NoiseNuisanceServiceRequest request)
-            => await PostAsync<string>($"{NoiseNuisanceEndpoint}", request);
+		public async Task<HttpResponse<string>> CreateNoiseNuisanceRequest(NoiseNuisanceServiceRequest request)
+			=> await PostAsync<string>($"{NoiseNuisanceEndpoint}", request);
 
-        public async Task<HttpResponse<string>> ResubmitNoiseNuisanceRequest(string crmCaseId)
-            => await PostAsync<string>($"{NoiseNuisanceEndpoint}/ResubmitCase", crmCaseId);
+		public async Task<HttpResponse<string>> ResubmitNoiseNuisanceRequest(string crmCaseId)
+			=> await PostAsync<string>($"{NoiseNuisanceEndpoint}/ResubmitCase", crmCaseId);
 
-        public async Task<HttpResponse<string>> CreateBonfireNuisanceRequest(BonfireNuisanceServiceRequest request)
-            => await PostAsync<string>($"{BonfireNuisanceEndpoint}", request);
+		public async Task<HttpResponse<string>> CreateBonfireNuisanceRequest(BonfireNuisanceServiceRequest request)
+			=> await PostAsync<string>($"{BonfireNuisanceEndpoint}", request);
 
-        public async Task<HttpResponse<string>> ResubmitBonfireNuisanceRequest(string crmCaseId)
-            => await PostAsync<string>($"{BonfireNuisanceEndpoint}/ResubmitCase", crmCaseId);
+		public async Task<HttpResponse<string>> ResubmitBonfireNuisanceRequest(string crmCaseId)
+			=> await PostAsync<string>($"{BonfireNuisanceEndpoint}/ResubmitCase", crmCaseId);
 
-        public async Task<HttpResponse<string>> CreateHousingDisrepairServiceRequest(HousingDisrepairServiceRequest request)
-            => await PostAsync<string>($"{HousingDisrepairEndpoint}", request);
+		public async Task<HttpResponse<string>> CreateHousingDisrepairServiceRequest(HousingDisrepairServiceRequest request)
+			=> await PostAsync<string>($"{HousingDisrepairEndpoint}", request);
 
-        public async Task<HttpResponse<string>> ResubmitHousingDisrepairServiceRequest(string crmCaseId)
-            => await PostAsync<string>($"{HousingDisrepairEndpoint}/ResubmitCase", crmCaseId);
+		public async Task<HttpResponse<string>> ResubmitHousingDisrepairServiceRequest(string crmCaseId)
+			=> await PostAsync<string>($"{HousingDisrepairEndpoint}/ResubmitCase", crmCaseId);
 
 		public async Task<HttpResponse<string>> CreateTaxiLicenceRequest(TaxiLicensingServiceRequest request)
 			=> await PostAsync<string>($"{TaxiLicenceEndpoint}", request);
@@ -63,7 +63,7 @@ namespace StockportGovUK.NetStandard.Gateways.UniformService
 		public async Task<HttpResponse<string>> UpdateTaxiLicenceRequestWithDetails(TaxiLicensingUpdateRequest request)
 			=> await PostAsync<string>($"{TaxiLicenceEndpointWithDetails}", request);
 
-		public async Task<HttpResponse<string>> CheckTaxiLicenceRequestIsOpen(TaxiLicensingUpdateRequest request) 
-            => await PostAsync<string>($"{TaxiLicenceEndpointCheckIsOpen}", request);
+		public async Task<HttpResponse<string>> CheckTaxiLicenceRequestIsOpen(TaxiLicensingUpdateRequest request)
+			=> await PostAsync<string>($"{TaxiLicenceEndpointCheckIsOpen}", request);
 	}
 }
