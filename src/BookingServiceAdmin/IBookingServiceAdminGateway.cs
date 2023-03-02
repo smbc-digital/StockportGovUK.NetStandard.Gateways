cd652385e7e053e8d87521b455bbbc98aeea0991
michaellau-smbc <62244533+michaellau-smbc@users.noreply.github.com>
2023-03-02 14:54:26 +0000

    chore(cancel): update response type to Note

--- src/BookingServiceAdmin/IBookingServiceAdminGateway.cs
+++ src/BookingServiceAdmin/IBookingServiceAdminGateway.cs
@@ -32,7 +32,7 @@ namespace StockportGovUK.NetStandard.Gateways.BookingServiceAdmin
         Task<HttpResponse<int>> GetDayBookingCountForContext(GetByDateRequest request);
         Task<HttpResponse<IEnumerable<Booking>>> GetNewAndConfirmedBookings(Guid contextId);
         Task<HttpResponse<Booking>> GetBooking(Guid bookingId);
-        Task<HttpResponse<bool>> CancelBooking(CancelBookingRequest request);
+        Task<HttpResponse<Note>> CancelBooking(CancelBookingRequest request);
 
         #endregion
 
