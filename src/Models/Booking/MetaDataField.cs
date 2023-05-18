03e69cc279e1cce17d6c9e1db54b5127c0f2153e
Lydia Gray <lydia.gray@stockport.gov.uk>
2023-05-18 14:49:13 +0100

    feat(Booking): Models for MetaData

--- src/Models/Booking/MetaDataField.cs
+++ src/Models/Booking/MetaDataField.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using StockportGovUK.NetStandard.Gateways.Enums;
 
 namespace StockportGovUK.NetStandard.Gateways.Models.Booking;
 
@@ -7,7 +8,7 @@ public class MetaDataField
     public Guid Id { get; set; }
     public string Key { get; set; }
     public string Name { get; set; }
-    public int Type { get; set; }
+    public EMetaDataType Type { get; set; }
     public bool IsMandatory { get; set; }
     public bool IsActive { get; set; }
     public Guid? AppointmentId { get; set; }
