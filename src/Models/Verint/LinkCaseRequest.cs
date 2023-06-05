9a460d93a1e3049825adb3e0c8f79eae3cd8c382
Lydia Gray <lydia.gray@stockport.gov.uk>
2023-06-05 13:58:24 +0100

    feat(Verint): add Model and Gateways for LinkCase and UnLinkCase

---
+++ src/Models/Verint/LinkCaseRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace StockportGovUK.NetStandard.Gateways.Models.Verint;
+public class LinkCaseRequest
+{
+    public long CaseReference { get; set; }
+    public long TargetCaseReference { get; set; }
+    public int LinkType { get; set; }
+}
