87c9c5aefb40e0abe4a0169a910ba2fb1a524fea
Lydia Gray <lydia.gray@stockport.gov.uk>
2023-08-11 09:21:15 +0100

    feat(WiredPlus): add new endpoints GetContactByEmail and Resubscribe

--- src/Models/WiredPlus/NewsletterSignUpResponse.cs
+++ src/Models/WiredPlus/NewsletterSignUpResponse.cs
@@ -2,7 +2,8 @@ namespace StockportGovUK.NetStandard.Gateways.Models.WiredPlus
 {
     public class NewsletterSignUpResponse
     {
-        public string id { get; set; }
-        public string message { get; set; }
+        public string Id { get; set; }
+        public string Message { get; set; }
+        public string Status { get; set; }
     }
 }
