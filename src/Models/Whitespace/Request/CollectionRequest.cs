1d2846e526df793866b2c290ea307f393e269be3
Lydia Gray <lydia.gray@stockport.gov.uk>
2023-11-17 09:13:26 +0000

    refactor(CollectionRequest): make dates nullable

--- src/Models/Whitespace/Request/CollectionRequest.cs
+++ src/Models/Whitespace/Request/CollectionRequest.cs
@@ -4,6 +4,6 @@ namespace StockportGovUK.NetStandard.Gateways.Models.Whitespace.Request;
 public class CollectionRequest
 {
     public string Uprn { get; set; }
-    public DateTime From { get; set; }
-    public DateTime To { get; set; }
+    public DateTime? From { get; set; }
+    public DateTime? To { get; set; }
 }
