02d8921a085eeaa62691447b3744e23d486fdbb4
Lydia Gray <lydia.gray@stockport.gov.uk>
2023-11-20 11:39:28 +0000

    fix(WhitespaceGateway): fix interface

--- src/WhitespaceService/IWhitespaceServiceGateway.cs
+++ src/WhitespaceService/IWhitespaceServiceGateway.cs
@@ -10,7 +10,7 @@ public interface IWhitespaceServiceGateway
     Task<HttpResponse<SiteResponse>> GetSiteInfoByUprn(string uprn);
     Task<HttpResponse<SiteIdResponse>> GetAccountSiteIdByUprn(string uprn);
     Task<HttpResponse<CollectionResponse>> GetCollectionByUprnAndDate(CollectionRequest request);
-    Task<HttpResponse<WorksheetResponse>> GetSiteWorksheets(string uprn);
+    Task<HttpResponse<WorksheetResponse>> GetSiteWorksheetsByUprn(string uprn);
     Task<HttpResponse<StreetResponse>> GetStreets(string postcode);
     Task<HttpResponse<InCabLogResponse>> GetInCabLogsByUsrn(InCabLogRequest request);
     Task<HttpResponse<InCabLogResponse>> GetInCabLogsByUprn(InCabLogRequest request);
