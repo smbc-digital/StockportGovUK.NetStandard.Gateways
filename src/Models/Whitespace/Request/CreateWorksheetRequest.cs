c89b75fe39d5edad4168ffae46b03c866f4f0a58
Lydia Gray <lydia.gray@stockport.gov.uk>
2023-11-30 13:04:08 +0000

    chore(Whitespace): tweaks to CreateWorksheetRequest

--- src/Models/Whitespace/Request/CreateWorksheetRequest.cs
+++ src/Models/Whitespace/Request/CreateWorksheetRequest.cs
@@ -1,16 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace StockportGovUK.NetStandard.Gateways.Models.Whitespace.Request;
 
 public class CreateWorksheetRequest
 {
+    [Required]
     public string Uprn { get; set; }
+    [Required]
     public int ServiceId { get; set; }
+    [Required]
     public string CaseReference { get; set; }
     public string Message { get; set; }
+    [Required]
     public DateTime StartDate { get; set; }
+    [Required]
     public IEnumerable<ServiceItemRequest> ServiceItems { get; set; }
     public IEnumerable<ServicePropertyRequest> ServiceProperties { get; set; }
-    public int AdHocRoundId { get; set; }
+    public int? AdHocRoundId { get; set; }
 }
