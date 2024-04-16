fc65f77099f5e430665fdfc5c7a1c026a091fee9
Jonathon Warwick <jonathon.warwick@stockport.gov.uk>
2024-04-16 10:54:13 +0100

    feat(conesso) add in models for conesso

---
+++ src/Models/Conesso/ContactRequest.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace StockportGovUK.NetStandard.Gateways.Models.Conesso
+{
+    public class ContactRequest
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        // PREFERRED NAME
+        public string PhoneNumber { get; set; }
+        public string Company { get; set; }
+        public string JobTitle { get; set; }
+        public string Address { get; set; }
+        public string Address2 { get; set; }
+        public string City { get; set; }
+        public string County { get; set; }
+        public string Postcode { get; set; }
+        public string CountryCode { get; set; } = "GBR";
+        public string Country { get; set; } = "United Kingdom";
+        public string Website { get; set; }
+        public string Gender { get; set; }
+        public string Tags { get; set; }
+        public List<KeyValuePair<string, string>> CustomValues { get; set; } = new List<KeyValuePair<string, string>>();
+        [Required]
+        public string OptIn { get; set; }
+        [Required]
+        public int ContactListId { get; set; }
+    }
+}
