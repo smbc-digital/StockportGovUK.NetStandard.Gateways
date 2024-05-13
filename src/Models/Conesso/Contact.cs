f77ff284a479131147c52f9ad9b5df110960574f
Jonathon Warwick <jonathon.warwick@stockport.gov.uk>
2024-05-13 11:36:57 +0100

    fix(contact): add lists to contact model

--- src/Models/Conesso/Contact.cs
+++ src/Models/Conesso/Contact.cs
@@ -1,10 +1,14 @@
-﻿namespace StockportGovUK.NetStandard.Gateways.Models.Conesso
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace StockportGovUK.NetStandard.Gateways.Models.Conesso
 {
     public class Contact
     {
         public int Id { get; set; }
         public string Email { get; set; }
         public string OptInStatus { get; set; }
+        public IEnumerable<List> Lists { get; set; }
         public string Error { get; set; }
     }
 }
