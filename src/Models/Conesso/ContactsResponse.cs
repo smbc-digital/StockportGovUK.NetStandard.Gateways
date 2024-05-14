5eb02f7146e2ad438e44b626a0c02d558850501d
Jonathon Warwick <jonathon.warwick@stockport.gov.uk>
2024-05-14 13:30:03 +0100

    fix(refactor): cleanup contacts and lists

---
+++ src/Models/Conesso/ContactsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace StockportGovUK.NetStandard.Gateways.Models.Conesso
+{
+    public class ContactsResponse
+    {
+        public IEnumerable<ContactResponse> Data {  get; set; }
+        public string Error { get; set; }
+        public IEnumerable<IEnumerable<string>> Message { get; set; }
+    }
+}
---
+++ src/Models/Conesso/ListsResponse.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace StockportGovUK.NetStandard.Gateways.Models.Conesso
+{
+    public class ListsResponse
+    {
+        public IEnumerable<ListResponse> Data { get; set; }
+        public string Error { get; set; }
+        public IEnumerable<IEnumerable<string>> Message { get; set; }
+    }
+}
