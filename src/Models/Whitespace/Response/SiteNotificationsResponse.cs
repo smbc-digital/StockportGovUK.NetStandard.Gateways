b833c8573b76338472a719616b3d80d0562017c9
Lydia Gray <lydia.gray@stockport.gov.uk>
2024-05-15 13:23:43 +0100

    feat(Whitespace): endpoint and models for GetSiteNotifications

---
+++ src/Models/Whitespace/Response/SiteNotificationsResponse.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace StockportGovUK.NetStandard.Gateways.Models.Whitespace.Response;
+public class SiteNotificationsResponse
+{
+    public List<SiteNotification> Notifications { get; set; }
+}
