445e29d4e7d41afb53cc325bbbc771c2d5051fc7
Lydia Gray <lydia.gray@stockport.gov.uk>
2024-07-11 11:16:56 +0100

    refactor(BookingAdmin): make int not nullable

--- src/Models/Booking/Request/UpdateUserPermissionRequest.cs
+++ src/Models/Booking/Request/UpdateUserPermissionRequest.cs
@@ -4,5 +4,5 @@ namespace StockportGovUK.NetStandard.Gateways.Models.Booking.Request;
 public class UpdateUserPermissionRequest : BaseUserRequest
 {
     public Guid ContextId { get; set; }
-    public int? Role { get; set; }
+    public int Role { get; set; }
 }
