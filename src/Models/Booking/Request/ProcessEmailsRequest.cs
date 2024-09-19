0612bc013cd227a330d570ef9eb6de72692c4390
Lydia Gray <lydia.gray@stockport.gov.uk>
2024-09-19 09:06:54 +0100

    chore(Bookings): add EmailAddressOverride to ProcessEmailsRequest

--- src/Models/Booking/Request/ProcessEmailsRequest.cs
+++ src/Models/Booking/Request/ProcessEmailsRequest.cs
@@ -5,4 +5,5 @@ namespace StockportGovUK.NetStandard.Gateways.Models.Booking.Request;
 public class ProcessEmailsRequest
 {
     public Guid BookingId { get; set; }
+    public string EmailAddressOverride { get; set; }
 }
