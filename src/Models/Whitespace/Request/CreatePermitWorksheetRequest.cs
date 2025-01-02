5c1188cecd08a30eed41c588fa63c724898d8afa
Lydia Gray <lydia.gray@stockport.gov.uk>
2025-01-02 07:46:25 +0000

    chore(CreatePermitWorksheetRequest): remove properies

--- src/Models/Whitespace/Request/CreatePermitWorksheetRequest.cs
+++ src/Models/Whitespace/Request/CreatePermitWorksheetRequest.cs
@@ -6,8 +6,6 @@ public class CreatePermitWorksheetRequest
     public CreateWorksheetRequest CreateWorksheetRequest { get; set; }
     public DateTime? EndDate { get; set; }
     public int Quantity { get; set; }
-    public int ExistingItemQuantity { get; set; }
-    public int SiteServiceId { get; set; } 
     public Verint.Address Address { get; set; }
     public string EmailAddress { get; set; }
     public string PhoneNumber { get; set; }
