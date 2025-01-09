cc23dd0646a9b6d57d5d1128096e50058281e696
Lydia Gray <lydia.gray@stockport.gov.uk>
2025-01-09 08:26:28 +0000

    chore(CreatePermitWorksheetRequest): make end date not nullable

--- src/Models/Whitespace/Request/CreatePermitWorksheetRequest.cs
+++ src/Models/Whitespace/Request/CreatePermitWorksheetRequest.cs
@@ -4,7 +4,7 @@ namespace StockportGovUK.NetStandard.Gateways.Models.Whitespace.Request;
 public class CreatePermitWorksheetRequest
 {
     public CreateWorksheetRequest CreateWorksheetRequest { get; set; }
-    public DateTime? EndDate { get; set; }
+    public DateTime EndDate { get; set; }
     public int Quantity { get; set; }
     public Verint.Address Address { get; set; }
     public string EmailAddress { get; set; }
