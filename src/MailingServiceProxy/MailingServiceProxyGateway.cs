8ee184291e063abc162fc03df9a9f705c90fa277
Lydia Gray <lydia.gray@stockport.gov.uk>
2025-04-08 07:46:24 +0100

    feat(MailingServiceProxy): new Gateway

---
+++ src/MailingServiceProxy/MailingServiceProxyGateway.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+using StockportGovUK.NetStandard.Gateways.Models.Mail;
+using StockportGovUK.NetStandard.Gateways.Response;
+
+namespace StockportGovUK.NetStandard.Gateways.MailingServiceProxy;
+
+public class MailingServiceProxyGateway : Gateway, IMailingServiceProxyGateway
+{
+    private const string MailingEndpoint = "api/v1/Home";
+    public MailingServiceProxyGateway(HttpClient httpClient) : base(httpClient)
+    {
+    }
+
+    public async Task<HttpResponse<string>> Send(Mail model)
+        => await PostAsync<string>(MailingEndpoint, model);
+}
