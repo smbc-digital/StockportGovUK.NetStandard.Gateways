3b4d9631b8a953a9c7f6b910cf26b046496d404a
Lydia Gray <lydia.gray@stockport.gov.uk>
2025-05-29 07:46:40 +0100

    chore(BookingsAdmin): slight name tweak for StatusGateway

--- src/BookingServiceAdmin/StatusAdminGateway.cs
+++ src/BookingServiceAdmin/StatusAdminGateway.cs
@@ -6,8 +6,8 @@ using StockportGovUK.NetStandard.Gateways.Response;
 namespace StockportGovUK.NetStandard.Gateways.BookingServiceAdmin;
 public partial class BookingServiceAdminGateway : Gateway, IBookingServiceAdminGateway
 {
-    private const string StatusesEndpoint = "api/v1/Statuses";
+    private const string StatusEndpoint = "api/v1/Status";
 
     public async Task<HttpResponse<IEnumerable<Status>>> GetSystemStatuses() =>
-        await GetAsync<IEnumerable<Status>>($"{StatusesEndpoint}/system");
+        await GetAsync<IEnumerable<Status>>($"{StatusEndpoint}/system");
 }
