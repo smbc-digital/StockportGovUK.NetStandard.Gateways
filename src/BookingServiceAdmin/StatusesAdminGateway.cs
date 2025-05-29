faa6204c463f93299443283ad0cb0aa07515eb40
Lydia Gray <lydia.gray@stockport.gov.uk>
2025-05-29 07:43:32 +0100

    refactor(BookingsAdmin): move and rename GetStatuses - breaking change

---
+++ src/BookingServiceAdmin/StatusesAdminGateway.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using StockportGovUK.NetStandard.Gateways.Models.Booking;
+using StockportGovUK.NetStandard.Gateways.Response;
+
+namespace StockportGovUK.NetStandard.Gateways.BookingServiceAdmin;
+public partial class BookingServiceAdminGateway : Gateway, IBookingServiceAdminGateway
+{
+    private const string StatusesEndpoint = "api/v1/Statuses";
+
+    public async Task<HttpResponse<IEnumerable<Status>>> GetSystemStatuses() =>
+        await GetAsync<IEnumerable<Status>>($"{StatusesEndpoint}/system");
+}
