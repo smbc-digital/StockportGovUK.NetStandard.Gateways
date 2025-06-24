e3cbaf7d89098f317de00e18ff437caaa9e922ad
Lydia Gray <lydia.gray@stockport.gov.uk>
2025-06-24 13:08:57 +0100

    chore(Booking BasicContext): add isDisabled to class

--- src/Models/Booking/BasicContext.cs
+++ src/Models/Booking/BasicContext.cs
@@ -8,4 +8,5 @@ public class BasicContext
     public string Name { get; set; }
     public string Description { get; set; }
     public string Icon { get; set; }
+    public bool IsDisabled { get; set; }
 }
