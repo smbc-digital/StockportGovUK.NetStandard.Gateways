1786df4eef852005414680d8ec5604cc95585bb6
Lydia Gray <lydia.gray@stockport.gov.uk>
2025-07-04 10:44:25 +0100

    chore(BookingAdmin): add AppointmentName to EmailTemplate

--- src/Models/Booking/EmailTemplate.cs
+++ src/Models/Booking/EmailTemplate.cs
@@ -10,6 +10,7 @@ public class EmailTemplate
     public string Subject { get; set; }
     public string Body { get; set; }
     public Guid? AppointmentId { get; set; }
+    public string AppointmentName { get; set; }
     public Guid? EmailTypeId { get; set; }
     public string EmailTypeName { get; set; }
 }
