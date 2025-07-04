74e33f5286db4dc2eb8124cbff6e5c985b515ba2
Lydia Gray <lydia.gray@stockport.gov.uk>
2025-07-04 06:57:22 +0100

    feat(Bookings): EmailTemplate model class

---
+++ src/Models/Booking/EmailTemplate.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace StockportGovUK.NetStandard.Gateways.Models.Booking;
+
+public class EmailTemplate
+{
+    public bool IsDefaultTemplate { get; set; }
+    public Guid ContextId { get; set; }
+    public Guid? TemplateId { get; set; }
+    public string Subject { get; set; }
+    public string Body { get; set; }
+    public Guid? AppointmentId { get; set; }
+    public Guid? EmailType { get; set; }
+}
