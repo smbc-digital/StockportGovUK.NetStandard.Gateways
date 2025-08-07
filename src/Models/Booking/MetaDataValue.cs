935b1fca0b72d9b765b15c5ae9a4b6ce1fbb357c
Lydia Gray <lydia.gray@stockport.gov.uk>
2025-08-07 13:45:47 +0100

    chore(Bookings): add extra fields to MetaDataCombined model

--- src/Models/Booking/MetaDataValue.cs
+++ src/Models/Booking/MetaDataValue.cs
@@ -6,6 +6,8 @@ public class MetaDataValue
 {
     public Guid Id { get; set; }
     public string Value { get; set; }
+    public DateTime DateTimeValue { get; set; }
+    public int IntValue { get; set; }
     public Guid? BookingId { get; set; }
     public Guid? MetaDataFieldId { get; set; }
     public Guid? MetaDataDropDownId { get; set; }
