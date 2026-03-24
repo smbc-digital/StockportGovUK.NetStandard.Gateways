b813df5a70c3164dfbdfab08ef170729412f9c85
Lydia Gray <lydia.gray@stockport.gov.uk>
2026-03-24 08:55:12 +0000

    feat(AdviceProGateway): add AdviceProGateway

---
+++ src/AdvicePro/AdviceProGateway.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StockportGovUK.NetStandard.Gateways.AdvicePro;
+
+public class AdviceProGateway : Gateway, IAdviceProGateway
+{
+    public AdviceProGateway(HttpClient httpClient) : base(httpClient)
+    {
+    }
+
+    public async Task<HttpResponseMessage> CreateReferral(object request)
+        => await PostAsync("referral", request);
+}
---
+++ src/AdvicePro/IAdviceProGateway.cs
@@ -0,0 +1,9 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StockportGovUK.NetStandard.Gateways.AdvicePro;
+
+public interface IAdviceProGateway
+{
+    Task<HttpResponseMessage> CreateReferral(object request);
+}
