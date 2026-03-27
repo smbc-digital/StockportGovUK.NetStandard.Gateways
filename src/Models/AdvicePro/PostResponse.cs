a30df378165f319794f8a58be2035217c2708bda
Lydia Gray <lydia.gray@stockport.gov.uk>
2026-03-27 06:53:14 +0000

    feat(AdvicePro): enhance Gateway and add models

---
+++ src/Models/AdvicePro/PostResponse.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace StockportGovUK.NetStandard.Gateways.Models.AdvicePro;
+
+public class PostResponse
+{
+    public int HttpStatusCode { get; set; }
+    public List<string> Messages { get; set; }
+    public int Identifier { get; set; }
+}
