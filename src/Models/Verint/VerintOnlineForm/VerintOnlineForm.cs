7cb78089376ce7e46bcd0171f7fffa2c51fb3bf9
Lydia Gray <lydia.gray@stockport.gov.uk>
2026-06-09 06:44:51 +0100

    chore(VerintOnlineFormRequest): test out different FormData Dictionarys

--- src/Models/Verint/VerintOnlineForm/VerintOnlineForm.cs
+++ src/Models/Verint/VerintOnlineForm/VerintOnlineForm.cs
@@ -4,4 +4,6 @@ namespace StockportGovUK.NetStandard.Gateways.Models.Verint.VerintOnlineForm;
 public class VerintOnlineForm
 {
     public Dictionary<string, string> FormData { get; set; }
+    public Dictionary<string, object> FormDataAllTypes { get; set; }
+    public Dictionary<string, string[]> FormDataCheckbox { get; set; }
 }
