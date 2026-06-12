13367a05c09a37c6d587e850800b423f4633f85d
Jonathan Hadley <jonathan.hadley@stockport.gov.uk>
2026-06-12 15:51:49 +0100

    patch() fix gateway interface issue

--- src/Netcall/ConverseCXGateway.cs
+++ src/Netcall/ConverseCXGateway.cs
@@ -5,25 +5,17 @@ using StockportGovUK.NetStandard.Gateways.Netcall.Models;
 
 namespace StockportGovUK.NetStandard.Gateways.Netcall
 {
-    public class ConverseCXGateway : Gateway, IConverseGateway
+    public class ConverseCXGateway : Gateway, IConverseCXGateway
     {
         private const string NETCALL_ENDPOINT = "/api/v1/recordings";
 
         public ConverseCXGateway(HttpClient httpClient) : base(httpClient)
         {}
         
-        public async Task<HttpResponseMessage> PauseRecordingAsync(string pid, string netcallUserId)
-            => await PutAsync(NETCALL_ENDPOINT, new ConverseCXRequestModel(ConverseCXRequestModel.RecordingStateValue.Pause, netcallUserId), true);
+        public async Task<HttpResponseMessage> PauseRecordingAsync(string agentId)
+            => await PutAsync(NETCALL_ENDPOINT, new ConverseCXRequestModel(ConverseCXRequestModel.RecordingStateValue.Pause, agentId), true);
 
-        public async Task<HttpResponseMessage> ResumeRecordingAsync(string pid, string netcallUserId)
-            => await PutAsync(NETCALL_ENDPOINT, new ConverseCXRequestModel(ConverseCXRequestModel.RecordingStateValue.Resume, netcallUserId), true);
-        
-        [Obsolete("Pausing recording from extension is not currently supported by Netcall Converse CX")]
-        public Task<HttpResponseMessage> PauseRecordingFromExtensionAsync(string pid, string extension)
-            => throw new NotImplementedException("Pausing recording from extension is not currently supported by Netcall Converse CX");
-
-        [Obsolete("Resuming recording from extension is not currently supported by Netcall Converse CX")]
-        public Task<HttpResponseMessage> ResumeRecordingFromExtensionAsync(string pid, string extension)
-            => throw new NotImplementedException("Resuming recording from extension is not currently supported by Netcall Converse CX");
+        public async Task<HttpResponseMessage> ResumeRecordingAsync(string agentId)
+            => await PutAsync(NETCALL_ENDPOINT, new ConverseCXRequestModel(ConverseCXRequestModel.RecordingStateValue.Resume, agentId), true);
     }
 }
---
+++ src/Netcall/IConverseCXGateway.cs
@@ -0,0 +1,12 @@
+﻿using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace StockportGovUK.NetStandard.Gateways.Netcall
+{
+    public interface IConverseCXGateway : IGateway
+    {
+        Task<HttpResponseMessage> PauseRecordingAsync(string agentId);   
+
+        Task<HttpResponseMessage> ResumeRecordingAsync(string agentId);
+    }
+}
